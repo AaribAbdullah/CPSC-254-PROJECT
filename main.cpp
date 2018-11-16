0bdee890370634b2b140b0699a611c664bc49bdb
Aarib Abdullah <aarib@csu.fullerton.edu>
2018-11-15 18:24:01 -0800

    Initial push, had some already completed

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+#include <QSplashScreen>
+#include <QTimer>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QSplashScreen *splash = new QSplashScreen;                      // added new
+    splash -> setPixmap(QPixmap("C:/Users/MD/Desktop/icon.ico"));   // added new
+    splash -> show();                                               // added new
+
+    MainWindow w;
+
+    QTimer::singleShot(10000, splash, SLOT(close()));   // added new
+    QTimer::singleShot(10000, &w, SLOT(show()));        // added new
+
+   // w.show();
+
+    return a.exec();
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,67 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString in_gas = ui->lineEdit->text();
+    double d_gas = in_gas.toDouble();
+
+
+    QString in_electric = ui->lineEdit_2->text();
+    double d_electric = in_electric.toDouble();
+
+    QString in_books = ui->lineEdit_3->text();
+    double d_books = in_books.toDouble();
+
+
+    QString in_carGas = ui->lineEdit_4->text();
+    double d_carGas = in_carGas.toDouble();
+
+    QString in_income = ui->lineEdit_5->text();
+    double d_income = in_income.toDouble();
+
+
+
+
+
+
+
+
+
+    double Result = d_income - (d_gas + d_electric + d_books + d_carGas);
+
+    //ui->label_7; will show result
+    QString result;
+
+    if (Result < 0){
+      result ="Your screwed! You could get another job or find a cheaper alternative way of living.";
+    }
+    else if (Result == 0.0){
+       result = "You may survive, but not enought for food.";
+    }
+    else if (Result > 0 && Result < 100){
+        result = "Now you can buy some \"light\" food.";
+    }
+    else if (Result > 100){
+      result = "I guess your alright.";
+    }
+
+    ui->label_7->setText(result);
+
+
+
+}
