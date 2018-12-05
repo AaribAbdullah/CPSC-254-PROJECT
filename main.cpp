d2bd112a6b8b3c0240fbd36465f6763aa84e2ecf
aatibabdullah <36347528+aatibabdullah@users.noreply.github.com>
2018-12-04 18:24:02 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,16 +11,17 @@ int main(int argc, char *argv[])
 
     QSplashScreen *splash = new QSplashScreen;                      // added new
     splash -> setPixmap(QPixmap("C:/Users/MD/Desktop/Splash_Screen.jpg"));   // added new
-
+    // we are bringing up a photo for the wait screen
     splash -> show();                                               // added new
 
     MainWindow w;
 
     QTimer::singleShot(10000, splash, SLOT(close()));   // added new
     QTimer::singleShot(10000, &w, SLOT(show()));        // added new
-
+// The top Qtimer for the wait scree that is in duration of 10000 miliseconds or 10 seconds
+// The the bottom QTimer is for the main window where we input our entries, in ten seconds
 
    //w.show();
 
-    return a.exec();
+    return a.exec(); // returns an execution object
 }
