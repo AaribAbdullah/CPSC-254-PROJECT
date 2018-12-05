71f6a9810d337aecac43963f4aaa568c7b57ccb0
Aarib Abdullah <aarib@csu.fullerton.edu>
2018-12-04 18:14:15 -0800

    Final code push, maybe?

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QSplashScreen *splash = new QSplashScreen;                      // added new
-    splash -> setPixmap(QPixmap("/home/ar-15/Desktop/Qt_GUI/test_gif.gif"));   // please change file type and path of file respectivley
+    splash -> setPixmap(QPixmap("/home/ar-15/Desktop/Qt_GUI/Splash_Screen.jpg"));   // please change file type and path of file respectivley
     splash -> show();                                               // added new
 
     MainWindow w;
